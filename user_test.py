0f20aba87118f893b98771edec39ed5d8dbf52c7
WayneMusungu <waynemuyera17@gmail.com>
2022-04-23 13:36:28 +0300

    Use the assertEqual to carry out the First test to ensure that the class object is being instatiated coorrectly

--- user_test.py
+++ user_test.py
@@ -17,5 +17,19 @@ class TestUser(unittest.TestCase):
         
         self.new_user = User("Wayne","Musungu","yyy@gmail.com","ORTIZ","12345") #Create user object
         
+    def test_init(self):
+        '''
+        test_init test case to test if the user object is initialized properly
+        '''
+        
+        self.assertEqual(self.new_user.first_name,"Wayne")
+        self.assertEqual(self.new_user.last_name,"Musungu")
+        self.assertEqual(self.new_user.email,"yyy@gmail.com")
+        self.assertEqual(self.new_user.username,"ORTIZ")
+        self.assertEqual(self.new_user.password,"12345")
+        
+        
+if __name__ == '__main__':
+    unittest.main()
     
     
