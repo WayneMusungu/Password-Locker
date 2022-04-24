429eaf0e9869d2c0b0666ee091566f15ceb7d153
WayneMusungu <waynemuyera17@gmail.com>
2022-04-24 05:49:33 +0300

    Create a login function for an existing user

--- run.py
+++ run.py
@@ -8,3 +8,12 @@ def create_user(fname,lname,uname,password):
     '''
     Function to create a new user
     '''
+    new_user = User(fname,lname,uname,password)
+    return new_user
+
+def login_user(username,password):
+    '''
+    Function that allows an existing user to login
+    '''
+    new_entry = User.verify_user(username,password)
+    return new_entry
